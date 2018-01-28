feb6ac159cdcfbcb7e71a7584bc28e7fdee77a71
pgrupa <pgrupa@move4mobile.com>
2018-01-28 14:39:29 +0100

    Some small changes

--- app/src/main/java/nl/menio/moneybunqer/MoneyBunqerConfiguration.kt
+++ app/src/main/java/nl/menio/moneybunqer/MoneyBunqerConfiguration.kt
@@ -2,4 +2,9 @@ package nl.menio.moneybunqer
 
 object MoneyBunqerConfiguration {
 
+    /**
+     * The base API URL to use.
+     */
+    const val API_URL = "https://sandbox.public.api.bunq.com/v1"
+
 }
